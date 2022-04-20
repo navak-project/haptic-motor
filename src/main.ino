6b172c74cf9980fe5ea5d68dcf1d74a93654f588
netherwaves <avery@netherwaves.com>
2022-04-19 23:43:28 -0400

    fix code

--- src/main.ino
+++ src/main.ino
@@ -9,46 +9,45 @@
 #include <AFMotor.h>
 
 AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
-AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
 
 int motorDur   = 200;
 int motorSpeed = 255;
 
+unsigned long time;
+bool motorActive = false;
+
 void setup()
 {
+  Serial.begin(9600);
+
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
 }
 
 void loop()
 {
-  
+  if (millis() - time > motorDur && motorActive) {
+    motorActive = false;
+    motor1.run(RELEASE);
+  }
 }
 
 
 void receiveEvent(int howMany)
 {
-  if (2 <= Wire.available()) 
+  if (2 <= Wire.available())
   {
     motorDur   = Wire.read() * 10;
     motorSpeed = Wire.read();
 
+    Serial.print(motorDur);
+    Serial.print(" ");
+    Serial.println(motorSpeed);
+
     motor1.setSpeed(motorSpeed);
     motor1.run(FORWARD);
-    motor2.setSpeed(motorSpeed);
-    motor2.run(FORWARD);
-    motor3.setSpeed(motorSpeed);
-    motor3.run(FORWARD);
-    motor4.setSpeed(motorSpeed);
-    motor4.run(FORWARD);
     
-    delay(motorDur);
-    
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    motor3.run(RELEASE);
-    motor4.run(RELEASE);
+    motorActive = true;
+    time = millis();
   }
 }  
